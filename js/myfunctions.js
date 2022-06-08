c7281f42779e52797c20493d26f7fb8ca621a9bd
Pietro Biondi <pietro.biondi94@gmail.com>
2022-06-08 11:36:24 +0200

    major changes

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -13,3 +13,35 @@ function showhidelist() {
      moreText.style.display = "inline";
   }
 }
+
+function showhidelist_pub() {
+   var dots = document.getElementById("dots_pub");
+   var moreText = document.getElementById("more_pub");
+   var btnText = document.getElementById("myBtn_pub");
+
+   if (dots.style.display === "none") {
+     dots.style.display = "inline";
+     btnText.innerHTML = "Read more"; 
+     moreText.style.display = "none";
+  } else {
+     dots.style.display = "none";
+     btnText.innerHTML = "Read less"; 
+     moreText.style.display = "inline";
+  }
+}
+
+function showhidelist_media() {
+   var dots = document.getElementById("dots_media");
+   var moreText = document.getElementById("more_media");
+   var btnText = document.getElementById("myBtn_media");
+
+   if (dots.style.display === "none") {
+     dots.style.display = "inline";
+     btnText.innerHTML = "Read more"; 
+     moreText.style.display = "none";
+  } else {
+     dots.style.display = "none";
+     btnText.innerHTML = "Read less"; 
+     moreText.style.display = "inline";
+  }
+}
